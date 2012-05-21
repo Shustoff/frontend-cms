285e11c91225d39fd1e683c3ef08a03714aa4245
Shustoff <dima.shustoff@gmail.com>
2012-05-21 20:22:19 +0400

    q

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -11,15 +11,14 @@
     <meta name="robots" content=“index,follow”>
     <title>Front-end CMS</title>
     <!--Стили-->
-    <?php echo HTML::style('assets/css/normalize.css');
-          echo HTML::style('assets/css/bootstrap.css');
+    <?php echo HTML::style('assets/css/bootstrap.css');
           echo HTML::style('assets/css/jquery-ui-1.8.16.custom.css');?>
     <link rel="stylesheet/less" type="text/css" href="<?=URL::base();?>assets/css/styles.less">
     <!--Скрипты-->
     <?php echo HTML::script('assets/js/less-1.3.0.min.js');
           echo HTML::script('assets/js/jquery-1.7.2.js');
-          echo HTML::script('assets/js/bootstrap.min.js');
           echo HTML::script('assets/js/jquery-ui-1.8.16.custom.min.js');
+          echo HTML::script('assets/js/bootstrap.min.js');
           echo HTML::script('assets/js/modernizr-2.5.3.js');
           echo HTML::script('assets/js/underscore.js');
           echo HTML::script('assets/js/backbone.js');
@@ -47,11 +46,11 @@
                     </div>
                     <div class="span11">
                         <ul class="menu">
-                            <li><a target="_blank" href="#">Главная</a></li>
-                            <li><a target="_blank" href="#">Настройки</a></li>
-                            <li><a target="_blank" href="#">Страницы</a></li>
-                            <li><a target="_blank" href="#">Пользователи</a></li>
-                            <li><a target="_blank" href="#">Модули</a></li>
+                            <li><a href="<?php echo URL::site('admin'); ?>">Главная</a></li>
+                            <li><a href="<?php echo URL::site('admin/blocks/options'); ?>">Настройки</a></li>
+                            <li><a href="<?php echo URL::site(); ?>">Страницы</a></li>
+                            <li><a href="<?php echo URL::site(); ?>">Пользователи</a></li>
+                            <li><a href="<?php echo URL::site(); ?>">Модули</a></li>
                         </ul>
                     </div>
                 </div>
@@ -67,148 +66,7 @@
 
         <div class="row">
             <div class="span16 main">
-                <div class="row">
-                    <div class="span8">
-                        <table class="table table-bordered table-stripped">
-                            <thead>
-                                <tr>
-                                    <th>Пользователи</th>
-                                    <th>IP адрес</th>
-                                    <th>Email</th>
-                                    <th>Роль</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>2.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>3.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>4.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>5.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>6.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>7.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>8.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>9.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                                <tr>
-                                    <td>10.</td>
-                                    <td>127.0.0.1</td>
-                                    <td>admin@admin.ru</td>
-                                    <td>администратор</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="span7">
-                        <h3>Последние добавленные страницы</h3>
-                        <table class="table table-striped table2">
-                            <thead>
-                                <tr>
-                                    <th>Дата создания</th>
-                                    <th>Название страницы</th>
-                                </tr>
-                            </thead>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                            <tr>
-                                <td>01-01-1900</td>
-                                <td>Название страницы...</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-                <div class="row">
-                    <h1>Инструменты: </h1>
-                    <ul class="tools">
-                        <li><a target="_blank" href="#"><img src=""/></a></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
+                <?php echo $main; ?>
             </div>
         </div>
 
