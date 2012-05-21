1305f82813341b7c8140b4dbe61ee25200d75abc
Shustoff <dima.shustoff@gmail.com>
2012-05-21 20:22:19 +0400

    q

--- application/classes/controller/admin/main.php
+++ application/classes/controller/admin/main.php
@@ -1,15 +1,37 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Admin_Main extends Controller {
+class Controller_Admin_Main extends Controller_Template {
+
+    public $template = '/admin/index';
 
 	public function action_index()
 	{
-
-
-
-
-
-        $this->response->body(View::factory('/admin/index'));
+        $main = View::factory('/admin/blocks/main');
+        $this->template->main = $main;
 	}
+
+    public function action_options()
+    {
+        $options = View::factory('/admin/blocks/options');
+        $this->template->main = $options;
+    }
+
+    public function action_pages()
+    {
+        $pages = View::factory('/admin/blocks/pages');
+        $this->template->main = $pages;
+    }
+
+    public function action_users()
+    {
+        $users = View::factory('/admin/blocks/users');
+        $this->template->main = $users;
+    }
+
+    public function action_modules()
+    {
+        $modules = View::factory('/admin/blocks/modules');
+        $this->template->main = $modules;
+    }
 
 } // End Welcome
---
+++ application/views/admin/blocks/main.php
@@ -0,0 +1,204 @@
+<div class="row">
+    <div class="span8">
+        <table class="table table-bordered table-stripped">
+            <thead>
+                <tr>
+                    <th>Пользователи</th>
+                    <th>IP адрес</th>
+                    <th>Email</th>
+                    <th>Роль</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>4.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>5.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>6.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>7.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>8.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>9.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+                <tr>
+                    <td>10.</td>
+                    <td>127.0.0.1</td>
+                    <td>admin@admin.ru</td>
+                    <td>администратор</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="span7">
+        <h3>Последние добавленные страницы</h3>
+        <table class="table table-striped table2">
+            <thead>
+                <tr>
+                    <th>Дата создания</th>
+                    <th>Название страницы</th>
+                </tr>
+            </thead>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+            <tr>
+                <td>01-01-1900</td>
+                <td>Название страницы...</td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="row">
+    <div class="span4">
+        <h1 class="h1">Инструменты: </h1>
+    </div>
+    <div class="span11">
+        <ul class="tools">
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/addpage.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/addcatalog.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/email.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/adduser.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/rss.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/stats.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/info.png');?>
+
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/youtube.png');?>
+
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/addimage.png');?>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="#">
+                    <?=HTML::image('assets/img/trash.png');?>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<hr class="line">
+<div class="row search">
+    <div class="span4"><p class="big">Поиск по страницам:</p></div>
+    <div class="span7"><input type="text" value="Поиск..." class="search-query search-input"></div>
+    <div class="span4"><input type="button" value="Поиск" class="btn"></div>
+</div>
+<div class="row search">
+    <div class="span4"><p class="big">Поиск по каталогам:</p></div>
+    <div class="span7"><input type="text" value="Поиск..." class="search-query search-input"></div>
+    <div class="span4"><input type="button" value="Поиск" class="btn"></div>
+</div>
+<div class="row search">
+    <div class="span4"><p class="big">Поиск по пользователям:</p></div>
+    <div class="span7"><input type="text" value="Поиск..." class="search-query search-input"></div>
+    <div class="span4"><input type="button" value="Поиск" class="btn"></div>
+</div>
