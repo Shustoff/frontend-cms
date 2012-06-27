891eb607cfdbc7e2150ee1bd4deee145cb6d68a9
Shustoff <dima.shustoff@gmail.com>
2012-06-27 19:40:43 +0400

    Merge branch 'master' of github.com:Shustoff/frontend-cms
    Conflicts:
    	assets/js/admin.js

--- frontend.sql
+++ frontend.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Июн 26 2012 г., 20:41
+-- Время создания: Июн 27 2012 г., 19:40
 -- Версия сервера: 5.5.9
 -- Версия PHP: 5.3.6
 
@@ -35,7 +35,7 @@ CREATE TABLE `catalogs` (
   `status` int(3) NOT NULL,
   `intrash` int(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Дамп данных таблицы `catalogs`
@@ -58,27 +58,51 @@ INSERT INTO `catalogs` VALUES(14, 'Каталог номер 13', 'catalog13', '
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `mails`
+--
+
+CREATE TABLE `mails` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `to` varchar(70) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `from` varchar(70) NOT NULL,
+  `message` text NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп данных таблицы `mails`
+--
+
+INSERT INTO `mails` VALUES(1, 'dima.shustoff@gmail.com', 'hello', 'admin@admin.ru', 'tryam!!!', '2012-06-27');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `modules`
 --
 
 CREATE TABLE `modules` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `desc` varchar(255) NOT NULL,
+  `systemname` varchar(50) NOT NULL,
+  `content` text NOT NULL,
   `user_id` int(5) NOT NULL,
+  `type` varchar(10) NOT NULL,
   `date` date NOT NULL,
   `status` int(3) NOT NULL,
   `intrash` int(3) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Дамп данных таблицы `modules`
 --
 
-INSERT INTO `modules` VALUES(2, 'Модуль навигации', 'Описание модуля навигации', 1, '2012-06-16', 1, 0);
-INSERT INTO `modules` VALUES(3, 'Модуль валидации', 'Описание модуля валидации', 1, '2012-06-16', 1, 0);
-INSERT INTO `modules` VALUES(5, 'Модуль пагинации', 'Описание модуля пагинации', 1, '2012-06-16', 1, 0);
+INSERT INTO `modules` VALUES(2, 'Модуль навигации', 'navigation', 'Описание модуля навигации', 1, 'HTML', '2012-06-16', 1, 0);
+INSERT INTO `modules` VALUES(3, 'Модуль валидации', 'validation', 'Описание модуля валидации', 1, 'PHP', '2012-06-16', 1, 0);
+INSERT INTO `modules` VALUES(5, 'Модуль пагинации', 'pagination', 'Описание модуля пагинации', 1, 'JS', '2012-06-16', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -153,9 +177,6 @@ INSERT INTO `pages` VALUES(18, 'страница 35', 'page4', 'Описание
 INSERT INTO `pages` VALUES(19, 'страница 35', 'page5', 'Описание страницы 35', 'ключевые слова, слова ключевые, словечки', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fringilla nibh quis odio sagittis imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut et odio neque, at accumsan ipsum. Phasellus ac bibendum massa. Donec egestas erat quis nunc consectetur scelerisque pretium tortor tristique. Suspendisse id diam ac ante ultricies convallis. Curabitur imperdiet porttitor est non scelerisque. Sed condimentum adipiscing ligula nec mollis. Nam risus nisi, pharetra nec rutrum quis, venenatis quis felis. Vivamus porta, turpis ut tempus pretium, erat mauris egestas nibh, eu tincidunt ante odio ac metus. Fusce tortor purus, vulputate et malesuada in, vehicula quis tellus. Nullam vestibulum ligula at nunc vulputate sit amet pulvinar nunc sollicitudin. Vestibulum dignissim placerat metus, in pellentesque odio viverra in.</p>\r\n\r\n<p>Vivamus rhoncus ultricies massa in sodales. Quisque ac purus nec nisl iaculis viverra. Praesent aliquet mauris a risus aliquet non suscipit nisi hendrerit. Fusce eu scelerisque eros. Pellentesque id eros in nisi aliquet malesuada. Morbi sodales tristique dictum. In hac habitasse platea dictumst. Integer vestibulum urna a tortor sollicitudin sit amet pharetra erat semper. Morbi eros quam, mattis non pellentesque a, ornare non risus. Duis accumsan dolor eget dui pharetra eu pharetra diam congue. Maecenas facilisis feugiat metus nec malesuada.</p>\r\n\r\n<p>Curabitur eget rutrum tortor. Duis eleifend libero ut dui sodales ullamcorper. Pellentesque commodo, arcu vel bibendum volutpat, velit eros dignissim lectus, eu egestas nunc sapien id dui. Curabitur ornare, velit vel imperdiet scelerisque, massa arcu commodo lacus, ac pellentesque urna nibh nec quam. Phasellus cursus ultrices scelerisque. Praesent magna leo, iaculis in pellentesque nec, volutpat ac massa. Nullam rhoncus consectetur rhoncus. Donec vel ultricies velit. Nam nec cursus lectus. Sed porta quam a sapien elementum quis volutpat lorem elementum. Phasellus eu feugiat ipsum. Nulla pretium magna in urna rutrum dignissim. Integer porta justo vitae sapien congue laoreet. Quisque ac lacus dolor. </p>', 1, 2, '2012-06-22', 1, 0);
 INSERT INTO `pages` VALUES(20, 'страница 36', 'page6', 'Описание страницы 36', 'ключевые слова, слова ключевые, словечки', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fringilla nibh quis odio sagittis imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut et odio neque, at accumsan ipsum. Phasellus ac bibendum massa. Donec egestas erat quis nunc consectetur scelerisque pretium tortor tristique. Suspendisse id diam ac ante ultricies convallis. Curabitur imperdiet porttitor est non scelerisque. Sed condimentum adipiscing ligula nec mollis. Nam risus nisi, pharetra nec rutrum quis, venenatis quis felis. Vivamus porta, turpis ut tempus pretium, erat mauris egestas nibh, eu tincidunt ante odio ac metus. Fusce tortor purus, vulputate et malesuada in, vehicula quis tellus. Nullam vestibulum ligula at nunc vulputate sit amet pulvinar nunc sollicitudin. Vestibulum dignissim placerat metus, in pellentesque odio viverra in.</p>\r\n\r\n<p>Vivamus rhoncus ultricies massa in sodales. Quisque ac purus nec nisl iaculis viverra. Praesent aliquet mauris a risus aliquet non suscipit nisi hendrerit. Fusce eu scelerisque eros. Pellentesque id eros in nisi aliquet malesuada. Morbi sodales tristique dictum. In hac habitasse platea dictumst. Integer vestibulum urna a tortor sollicitudin sit amet pharetra erat semper. Morbi eros quam, mattis non pellentesque a, ornare non risus. Duis accumsan dolor eget dui pharetra eu pharetra diam congue. Maecenas facilisis feugiat metus nec malesuada.</p>\r\n\r\n<p>Curabitur eget rutrum tortor. Duis eleifend libero ut dui sodales ullamcorper. Pellentesque commodo, arcu vel bibendum volutpat, velit eros dignissim lectus, eu egestas nunc sapien id dui. Curabitur ornare, velit vel imperdiet scelerisque, massa arcu commodo lacus, ac pellentesque urna nibh nec quam. Phasellus cursus ultrices scelerisque. Praesent magna leo, iaculis in pellentesque nec, volutpat ac massa. Nullam rhoncus consectetur rhoncus. Donec vel ultricies velit. Nam nec cursus lectus. Sed porta quam a sapien elementum quis volutpat lorem elementum. Phasellus eu feugiat ipsum. Nulla pretium magna in urna rutrum dignissim. Integer porta justo vitae sapien congue laoreet. Quisque ac lacus dolor. </p>', 1, 2, '2012-06-22', 1, 0);
 INSERT INTO `pages` VALUES(21, 'Страница 2', 'page222', 'Описание страницы 2', 'ключевые слова, слова ключевые, словечки', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fringilla nibh quis odio sagittis imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut et odio neque, at accumsan ipsum. Phasellus ac bibendum massa. Donec egestas erat quis nunc consectetur scelerisque pretium tortor tristique. Suspendisse id diam ac ante ultricies convallis. Curabitur imperdiet porttitor est non scelerisque. Sed condimentum adipiscing ligula nec mollis. Nam risus nisi, pharetra nec rutrum quis, venenatis quis felis. Vivamus porta, turpis ut tempus pretium, erat mauris egestas nibh, eu tincidunt ante odio ac metus. Fusce tortor purus, vulputate et malesuada in, vehicula quis tellus. Nullam vestibulum ligula at nunc vulputate sit amet pulvinar nunc sollicitudin. Vestibulum dignissim placerat metus, in pellentesque odio viverra in.</p>\r\n\r\n<p>Vivamus rhoncus ultricies massa in sodales. Quisque ac purus nec nisl iaculis viverra. Praesent aliquet mauris a risus aliquet non suscipit nisi hendrerit. Fusce eu scelerisque eros. Pellentesque id eros in nisi aliquet malesuada. Morbi sodales tristique dictum. In hac habitasse platea dictumst. Integer vestibulum urna a tortor sollicitudin sit amet pharetra erat semper. Morbi eros quam, mattis non pellentesque a, ornare non risus. Duis accumsan dolor eget dui pharetra eu pharetra diam congue. Maecenas facilisis feugiat metus nec malesuada.</p>\r\n\r\n<p>Curabitur eget rutrum tortor. Duis eleifend libero ut dui sodales ullamcorper. Pellentesque commodo, arcu vel bibendum volutpat, velit eros dignissim lectus, eu egestas nunc sapien id dui. Curabitur ornare, velit vel imperdiet scelerisque, massa arcu commodo lacus, ac pellentesque urna nibh nec quam. Phasellus cursus ultrices scelerisque. Praesent magna leo, iaculis in pellentesque nec, volutpat ac massa. Nullam rhoncus consectetur rhoncus. Donec vel ultricies velit. Nam nec cursus lectus. Sed porta quam a sapien elementum quis volutpat lorem elementum. Phasellus eu feugiat ipsum. Nulla pretium magna in urna rutrum dignissim. Integer porta justo vitae sapien congue laoreet. Quisque ac lacus dolor. </p>', 1, 2, '2012-06-14', 1, 0);
-INSERT INTO `pages` VALUES(55, 'sdfsdf', 'sdfsff', 'sdfs', 'sdfsdfs', 'sdfsdfsd', 11, 1, '2012-06-01', 1, 0);
-INSERT INTO `pages` VALUES(54, 'werwer', 'werwe', 'werwer', 'werwerwe', 'rwerwerwer', 5, 1, '2012-06-27', 1, 0);
-INSERT INTO `pages` VALUES(56, '', '', '', '', '', 0, 0, '0000-00-00', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -166,16 +187,23 @@ INSERT INTO `pages` VALUES(56, '', '', '', '', '', 0, 0, '0000-00-00', 1, 0);
 CREATE TABLE `roles` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `rolename` varchar(30) NOT NULL,
+  `pages` int(2) NOT NULL,
+  `cats` int(2) NOT NULL,
+  `users` int(2) NOT NULL,
+  `mods` int(2) NOT NULL,
+  `mails` int(2) NOT NULL,
+  `stats` int(2) NOT NULL,
+  `opts` int(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Дамп данных таблицы `roles`
 --
 
-INSERT INTO `roles` VALUES(1, 'Администратор');
-INSERT INTO `roles` VALUES(2, 'Модератор');
-INSERT INTO `roles` VALUES(3, 'Зарегистрированный');
+INSERT INTO `roles` VALUES(1, 'Администратор', 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO `roles` VALUES(2, 'Модератор', 1, 0, 0, 0, 1, 1, 0);
+INSERT INTO `roles` VALUES(3, 'Зарегистрированный', 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
