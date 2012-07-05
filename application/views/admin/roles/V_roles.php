c6724bd6e016dc433eea51a45bc6ab084f5c4b67
Shustoff <dima.shustoff@gmail.com>
2012-07-05 20:57:42 +0400

    Merge branch 'master' of github.com:Shustoff/frontend-cms
    Conflicts:
    	assets/js/admin.js

---
+++ application/views/admin/roles/V_roles.php
@@ -0,0 +1,97 @@
+<h3>Все модули</h3>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Название</th>
+            <th>Описание</th>
+            <th width="50">Состояние</th>
+            <th width="80">В корзину</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($roles as $role) : ?>
+            <tr class="pageedit">
+                <td><?=$role->id;?></td>
+                <td class="pagename">
+                    <form action="" method="post" name="edititem" id="edititem">
+                        <input type="hidden" value="<?=$role->id;?>" name="id">
+                        <a href="#" onclick="req.editItem('roles',<?=$role->id;?>);">
+                            <?=$role->name;?>
+                        </a>
+                    </form>
+                </td>
+                <td><?=$role->description;?></td>
+                <td>
+                    <form action="" method="post" id="cngstatus<?=$role->id;?>">
+                        <input type="hidden" name="idpage" value="<?=$role->id;?>">
+                        <?php if ($role->status == 1) : ?>
+                             <a href="#" onclick="req.off('roles',this); return false;"><img src='<?=URL::base()?>assets/img/published.png'></a>
+                        <? else : ?>
+                             <a href="#" onclick="req.on('roles',this); return false;"><img src='<?=URL::base()?>assets/img/not-published.png'></a>
+                        <? endif; ?>
+                    </form>
+                </td>
+                <td>
+                    <form action="" method="post" id="intrash<?=$role->id;?>">
+                        <input type="hidden" name="intrash" value="<?=$role->id;?>">
+                        <a href="#" onclick="req.intrash('roles',this);"><img src='<?=URL::base()?>assets/img/delete.png'></a>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<form action="" method="post" id="save">
+<div class="row">
+    <div class="span4">
+        <div class="control-group">
+            <label class="control-label" for="select1">Сортировать по:</label>
+            <div class="controls">
+                <select id="select1" name="sortby" class="input-medium">
+                    <option value="id">ID</option>
+                    <option value="name">Названию</option>
+                    <option value="description">Описанию</option>
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="span4">
+        <div class="control-group">
+            <label class="control-label" for="select2">Выводить по:</label>
+            <div class="controls">
+                <select id="select2" name="limit" class="input-medium">
+                    <option>5</option>
+                    <option>10</option>
+                    <option>15</option>
+                    <option>20</option>
+                    <option>25</option>
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="span3 savepages">
+        <a class="btn btn-success" onclick="req.sortItems('roles');">Применить настройки</a>
+    </div>
+</div>
+</form>
+
+<div class="row">
+    <div class="span12">
+        <div class="pagination">
+            <ul>
+                <?php for ($i = 1; $i <= $count; $i++ ) : ?>
+                <li>
+                    <form action="" method="post" id="pagination<?=$i;?>">
+                        <input type="hidden" name="offset" id="offset" value="">
+                        <input type="hidden" name="limit" id="limit" value="">
+                        <input type="hidden" name="sortby" id="sortby" value="">
+                        <a href="#" class="pageN" name="pagination" onclick="req.pagination('roles',this);"><?=$i;?></a>
+                    </form>
+                </li>
+                <? endfor; ?>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ application/views/admin/users/V_edituser.php
@@ -0,0 +1,83 @@
+<h3 class="center">Изменить пользователя</h3>
+<form action="" method="post" id="edititem">
+<div class="row">
+    <div class="span6">
+        <div class="control-group">
+            <label class="control-label" for="username">Логин:</label>
+            <div class="controls">
+                <input type="text" id="username" name="username" class="input-xlarge" value="<?=$user->username;?>">
+            </div>
+        </div>
+    </div>
+    <div class="span5 aliasright">
+        <div class="control-group">
+            <label class="control-label" for="email">Email:</label>
+            <div class="controls">
+                <input type="text" id="email" name="email" class="input-xlarge" value="<?=$user->email;?>">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="span6">
+        <div class="control-group">
+            <label class="control-label" for="password">Пароль</label>
+            <div class="controls">
+                <input type="password" id="password" name="password" class="input-xlarge" value="">
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="passwordconfirm">Повтор пароля</label>
+            <div class="controls">
+                <input type="password" id="passwordconfirm" name="password_confirm" class="input-xlarge" value="">
+            </div>
+        </div>
+    </div>
+    <div class="span5 aliasright">
+        <div class="control-group">
+            <label class="control-label" for="firstname">Имя:</label>
+            <div class="controls">
+                <input type="text" id="firstname" name="firstname" class="input-xlarge" value="<?=$user->firstname;?>">
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="surname">Фамилия</label>
+            <div class="controls">
+                <input type="text" id="surname" name="lastname" class="input-xlarge" value="<?=$user->lastname;?>">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="span6">
+        <div class="control-group">
+            <label class="control-label" for="role">Группа:</label>
+            <div class="controls">
+                <select id="role" class="input-xlarge" name="role_name">
+                    <?php foreach($roles as $role) : ?>
+                        <option value="<?=$role->name; ?>" <?=$role->name == $userrole ? 'selected' : '' ?>>
+                            <?=$role->description; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+        </div>
+    </div>
+    <script>
+    	$(function() {
+    		$( "#datepicker" ).datepicker();
+    	});
+    </script>
+    <div class="span5 aliasright">
+        <div class="control-group">
+            <label class="control-label" for="datepicker">Дата создания:</label>
+            <div class="controls">
+                <input id="datepicker" type="text" name="datereg" class="input-small" value="<?=$user->datereg;?>">
+                <input type="hidden" name="status" value="<?=$user->status;?>">
+                <input type="hidden" name="id" value="<?=$user->id;?>">
+                <a class="btn btn-success btncheck" href="#" onclick="req.edit('users');return false;">Сохранить</a>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
