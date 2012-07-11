c09fd8aa25e225db0ddae7fb1235506b31156546
Shustoff <dima.shustoff@gmail.com>
2012-07-11 19:53:54 +0400

    new commit

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -14,6 +14,7 @@
     <meta name="robots" content="<?=$robots;?>">
     <meta name="description" content="<?=$description;?>">
     <meta name="keywords" content="<?=$keywords;?>">
+    <meta name="copyright" content="<?=$copyright;?>">
 
     <!--Стили-->
     <link rel="stylesheet" href="<?=URL::base();?>assets/css/h5bp.css">
@@ -24,7 +25,6 @@
     <script src="<?=URL::base();?>assets/js/site/json2.js"></script>
     <script src="<?=URL::base();?>assets/js/site/underscore.js"></script>
     <script src="<?=URL::base();?>assets/js/site/backbone.js"></script>
-    <script src="<?=URL::base();?>assets/js/site/plugins.js"></script>
     <script src="<?=URL::base();?>assets/js/site/site.js"></script>
 
     <!--Фавикон-->
@@ -42,14 +42,14 @@
 
 
     <!--Подключаем jQuery с Google CDN-->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
     <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+    <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
 </body>
 </html>
