281cdc7883392a362ee0ed01a1e1f157ac8dd0de
Dmitriy <dima.shustoff@gmail.com>
2012-08-04 17:36:47 +0400

    sync

--- assets/js/site/site.js
+++ assets/js/site/site.js
@@ -2,21 +2,13 @@
  * Главный модуль сайта
  */
 
-<<<<<<< HEAD
+
 define([
     'jQuery',
     'Underscore',
     'Backbone',
     'site/route'
 ], function ($, _, Backbone, Router) {
-=======
-require([
-    'libs/jquery/jquery',
-    'libs/underscore/underscore',
-    'libs/backbone/backbone',
-    'site/models/pages'
-], function ($, _, Backbone, Page) {
->>>>>>> a7804172e69fadd9f9835184fda650d899d2dfff
 
     var initialize = function () {
         Router.initialize();
