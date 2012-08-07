776010c0cab1ff0fb6e4580ffe521d0381b6239d
Shustoff <dima.shustoff@gmail.com>
2012-08-07 20:24:26 +0400

    Succesfully QUnit testing integration

--- assets/js/site/site.js
+++ assets/js/site/site.js
@@ -1,8 +1,27 @@
 /**
- * Главный модуль сайта. Здесь пишем все свои скрипты.
+ * Входная точка для всех js скриптов. Bootstrap.
  */
+requirejs.config({
 
-define([
+    deps : ['site', 'tests'],
+    paths : {
+        // Libs
+        'jQuery' : '../libs/jquery/jquery-1.7.2',
+        'Underscore' : '../libs/underscore',
+        'Backbone' : '../libs/backbone',
+        'QUnit' : '../libs/qunit-1.9.0',
+        'text' : '../libs/require/text'
+    },
+    shim : {
+        'Backbone' : {
+            deps : ['jQuery', 'Underscore'],
+            exports : 'Backbone'
+        }
+    }
+    
+});
+
+require([
     'router',
     'collections/catalogs',
     'models/pages',
@@ -14,6 +33,5 @@ define([
 
         var catalogView = new CatalogView({collection : catalog});
         $('.mid').append( catalogView.render().el );
-
 
 });
