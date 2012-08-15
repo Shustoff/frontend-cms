2ae2df0853a4bad3010c8f6edc8b1d258cfd70bf
Dmitriy <dima.shustoff@gmail.com>
2012-08-15 12:32:59 +0400

    adding cache

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -100,7 +100,7 @@ Kohana::$config->attach(new Config_File);
 Kohana::modules(array(
 	'auth'       => MODPATH.'auth',       // Basic authentication
 	'cache'      => MODPATH.'cache',      // Caching with multiple backends
-	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
+    'codebench'  => MODPATH.'codebench',  // Benchmarking tool
 	'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
