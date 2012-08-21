fc6e9ee8ed681310332c0eba3c52642c958e76ab
Shustoff <dima.shustoff@gmail.com>
2012-08-21 19:08:03 +0400

    added files

--- application/views/site/blocks/V_nav.php
+++ application/views/site/blocks/V_nav.php
@@ -1,11 +1,11 @@
 <!-- Меню -->
 <nav class="topmenu row">
     <ul>
-        <li><a href="c/catalog1">Каталог 1</a></li>
-        <li><a href="c/catalog2">Каталог 2</a></li>
-        <li><a href="c/catalog3">Каталог 3</a></li>
-        <li><a href="c/catalog4">Каталог 4</a></li>
-        <li><a href="c/catalog5">Каталог 5</a></li>
+        <li><a href="catalog1/">Каталог 1</a></li>
+        <li><a href="catalog2/">Каталог 2</a></li>
+        <li><a href="catalog3/">Каталог 3</a></li>
+        <li><a href="catalog4/">Каталог 4</a></li>
+        <li><a href="catalog5/">Каталог 5</a></li>
         <div class="clear"></div>
     </ul>
 </nav>
